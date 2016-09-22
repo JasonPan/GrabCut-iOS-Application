a0ec881186164beb72f714a6c2f8d2073e81278e
Jason Pan <jason@antdevelopment.com>
2016-09-22 13:59:43 +1000

    Initial commit

---
+++ ios app/grabcutios-master/GrabCutIOS/GrabCutIOS/ViewController.h
@@ -0,0 +1,22 @@
+//
+//  ViewController.h
+//  GrabCutIOS
+//
+//  Created by EunchulJeon on 2015. 8. 29..
+//  Copyright (c) 2015 Naver Corp.
+//  @Author Eunchul Jeon
+//
+
+#import <UIKit/UIKit.h>
+
+#import <opencv2/opencv.hpp>
+#import <opencv2/imgproc/imgproc_c.h>
+#import <opencv2/highgui/highgui_c.h>
+#import <opencv2/core/core_c.h>
+using namespace cv;
+
+@interface ViewController : UIViewController
+
+
+@end
+
